362ada1edf156c0d07ff1a2b9d1552cb2fae3f38
th3-st3w4rd <ChristianS.Green@gmail.com>
2025-04-15 08:14:31 -0400

    add: add telescope file-browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
@@ -21,9 +22,45 @@ return {
 
     },
     {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            -- This is your opts table
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                            -- even more opts
+                        }
+
+                        -- pseudo code / specification for writing custom displays, like the one
+                        -- for "codeactions"
+                        -- specific_opts = {
+                        --   [kind] = {
+                        --     make_indexed = function(items) -> indexed_items, width,
+                        --     make_displayer = function(widths) -> displayer
+                        --     make_display = function(displayer) -> function(e)
+                        --     make_ordinal = function(e) -> string
+                        --   },
+                        --   -- for example to disable the custom builtin "codeactions" display
+                        --      do the following
+                        --   codeactions = false,
+                        -- }
+                    }
+                }
+            })
+            -- To get ui-select loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require("telescope").load_extension("ui-select")
+        end
+    },
+    {
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
+            --local tsbrowser = require("telescope")
+            --tsbrowser.setup({
+            --local fb_actions = require "telescope.extensions.file_browser.actions"
+            -- local fb_picker = require "telescope.exntensions.file_browser.picker"
             require("telescope").setup({
                 -- You don't need to set any of these options.
                 -- IMPORTANT!: this is only a showcase of how you can set default options!
@@ -38,15 +75,15 @@ return {
                                 -- your custom insert mode mappings
                             },
                             ["n"] = {
-                                --["<leader>fb"] = tsbrowser.file_browser, -- your custom normal mode mappings
+                                --["<leader>fb"] = fb_picker.file_browser, -- your custom normal mode mappings
                             },
                         },
                     },
                 },
             })
+            -- To get telescope-file-browser loaded and working with telescope,
+            -- you need to call load_extension, somewhere after setup function:
             require("telescope").load_extension("file_browser")
         end
-        -- To get telescope-file-browser loaded and working with telescope,
-        -- you need to call load_extension, somewhere after setup function:
     },
 }
