b429075a6b4fa1663159a86ae1720948193aa44a
th3-st3w4rd <ChristianS.Green@gmail.com>
2025-04-15 08:16:30 -0400

    fix: resolve python lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,6 +14,7 @@ return {
                     "gopls",
                     "superhtml",
                     "marksman",
+                    "pylsp",
                     "jedi_language_server",
                     "textlsp",
                     "harper_ls",
